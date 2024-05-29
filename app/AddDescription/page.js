07a8661ab92ce1adf2e6b81566b972ed29ba28ad
HamzaLatif8712 <hamza1958712@gmail.com>
2024-05-29 14:56:06 +0500

    05/29_updates

---
+++ app/AddDescription/page.js
@@ -0,0 +1,235 @@
+'use client'
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
+import Snackbar from '@mui/material/Snackbar';
+import { Alert } from '@mui/material';
+import Slide from '@mui/material/Slide';
+import { comment } from 'postcss';
+
+const Page = () => {
+
+    const [EmailValue, setEmailValue] = useState('');
+    const [NameValue, setNameValue] = useState('');
+    const [DescriptionValue, setDescriptionValue] = useState('');
+    const [error, setError] = useState(false);
+    const [ShowResponse, setShowResponse] = useState(false);
+    const [Data, setData] = useState('')
+    const [Loading, setLoading] = useState(false)
+
+    console.log('Value of email is', EmailValue)
+
+    //api link
+    //https://plurawlapp.com/plurawl/api/users/contact_us
+
+    const Label = {
+        fontWeight: '600',
+        // fontFamily: customFonts.medium,
+        fontSize: 18,
+        color: '#ffffff',
+        marginTop: 30
+    }
+
+    // if (EmailValue.length === 0) {
+    //     setError(true)
+    //     console.log('Enter email')
+    // } else if (NameValue.length === 0) {
+    //     setError(true)
+    //     console.log('Enter name value')
+    // }
+
+    const handleContactUs = async () => {
+        if (EmailValue.length === 0) {
+            setError(true)
+            console.log('Enter email')
+        } else if (NameValue.length === 0) {
+            setError(true)
+            console.log('Enter name value')
+        } else if (DescriptionValue.length === 0) {
+            setError(true)
+        } else {
+            try {
+                setLoading(true);
+                const apiUrl = "https://plurawlapp.com/plurawl/api/users/contact_us";
+                const data = localStorage.getItem('user')
+                const d = JSON.parse(data);
+                const response = await fetch(apiUrl, {
+                    'method': 'post',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        // 'Authorization': 'Bearer ' + d.token
+                    },
+                    body: JSON.stringify({
+                        email: EmailValue,
+                        name: NameValue,
+                        comment: DescriptionValue
+                    })
+                });
+
+
+                if (response.ok) {
+                    const Result = await response.json();
+                    console.log('Response is', Result)
+                    setData(Result);
+                }
+
+
+            } catch (error) {
+                console.log('Error is', error)
+            } finally {
+                setShowResponse(true)
+                setLoading(false)
+            }
+        }
+    }
+
+    const handleClose = () => {
+        setError(false);
+        setShowResponse(false);
+    }
+
+    return (
+        <div className="w-full" style={{ backgroundColor: 'black', height: '100vh', display: 'flex', justifyContent: 'center' }}>
+            <div style={{ width: '350px', display: 'flex', flexDirection: 'column', color: 'white', marginTop: 50 }}>
+                <Snackbar
+                    open={error}
+                    autoHideDuration={5000}
+                    onClose={handleClose}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    TransitionComponent={Slide}
+                    TransitionProps={{
+                        direction: 'left',
+                    }}
+                >
+                    <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+                        Enter all credentials
+                    </Alert>
+                </Snackbar>
+
+                <Snackbar
+                    open={ShowResponse}
+                    autoHideDuration={2500}
+                    onClose={handleClose}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    TransitionComponent={Slide}
+                    TransitionProps={{
+                        direction: 'left',
+                    }}
+                >
+                    <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+                        {Data.message}
+                    </Alert>
+                </Snackbar>
+
+                <p style={Label}>
+                    Email
+                </p>
+
+                <div>
+                    <TextField
+                        id="outlined-basic"
+                        label="Email"
+                        variant="outlined"
+                        value={EmailValue}
+                        onChange={(e) => setEmailValue(e.target.value)}
+                        sx={{
+                            '& .MuiInputBase-root': {
+                                color: '#ffffff',  // Text color
+                                width: '350px', // Field width
+                            },
+                            '& .MuiOutlinedInput-notchedOutline': {
+                                borderColor: '#ffffff', // Outline color
+                                borderRadius: 3
+                            },
+                            '& .MuiInputLabel-root': {
+                                color: '#ffffff', // Label color
+                            },
+                            marginTop: 2
+                        }}
+                    />
+                </div>
+
+                <p style={Label}>
+                    Name
+                </p>
+
+                <div>
+                    <TextField
+                        id="outlined-basic"
+                        label="Name"
+                        variant="outlined"
+                        value={NameValue}
+                        onChange={(e) => setNameValue(e.target.value)}
+                        sx={{
+                            '& .MuiInputBase-root': {
+                                color: '#ffffff',  // Text color
+                                width: '350px', // Field width
+                            },
+                            '& .MuiOutlinedInput-notchedOutline': {
+                                borderColor: '#ffffff', // Outline color
+                                borderRadius: 3
+                            },
+                            '& .MuiInputLabel-root': {
+                                color: '#ffffff', // Label color
+                            },
+                            marginTop: 2
+                        }}
+                    />
+                </div>
+
+                <p style={Label}>
+                    Description
+                </p>
+
+                <div>
+                    <TextField
+                        id="outlined-multiline-static"
+                        label="Description"
+                        multiline
+                        rows={4}
+                        value={DescriptionValue}
+                        onChange={(e) => setDescriptionValue(e.target.value)}
+                        // defaultValue="Add Description"
+                        sx={{
+                            '& .MuiInputBase-root': {
+                                color: '#ffffff',  // Text color
+                                width: '350px', // Field width
+                            },
+                            '& .MuiOutlinedInput-notchedOutline': {
+                                borderColor: '#ffffff', // Outline color
+                                borderRadius: 3
+                            },
+                            '& .MuiInputLabel-root': {
+                                color: '#ffffff', // Label color
+                            },
+                            marginTop: 2
+                        }}
+                    />
+                </div>
+
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    {Loading ?
+                        <div style={{ marginTop: 50, }}>
+                            <CircularProgress sx={{ height: '30px' }} />
+                        </div> :
+                        <div>
+                            <Button onClick={handleContactUs} className='font-semibold' variant='contained' style={{ marginTop: 50, height: 50, fontSize: 20 }}>
+                                Contact us
+                            </Button>
+                        </div>
+                    }
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Page
--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 'use client'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { React, useState, useEffect, Suspense } from 'react'
 import Snackbar from '@mui/material/Snackbar';
-import { Alert } from '@mui/material';
+import { Alert, Button } from '@mui/material';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import { Result } from 'postcss';
@@ -17,6 +17,60 @@ import axios from 'axios';
 const Page = () => {
 
   const router = useRouter()
+
+  const searchParams = useSearchParams();
+  const [WebCodeError, setWebCodeError] = useState(false)
+  const [WebCodeResponse, setWebCodeResponse] = useState('')
+  const [WebCodeLoading, setWebCodeLoading] = useState(false)
+
+  useEffect(() => {
+    const handleUrlCode = async () => {
+      const code = searchParams.get("code");
+      console.log("Code is ", code);
+      try {
+        setWebCodeLoading(true);
+        // const apiUrl = process.env.NEXT_PUBLIC_REACT_APP_ENVIRONMENT === "Development2" ? "https://bf59-119-156-82-235.ngrok-free.app/api/users/verify_web_token" : "https://plurawlapp.com/plurawl/api/users/verify_web_token"
+        const apiUrl = "https://plurawlapp.com/plurawl/api/users/verify_web_token";
+        // const AuthToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxMjUsIm5hbWUiOiJUcnVtcDEiLCJlbWFpbCI6InRydW1wMUBwbHVyYXdsLmNvbSIsInBhc3N3b3JkIjoiJDJiJDEwJElQVDBQOGtLWUE3a2V5d3hGdkcybE9HNmdTOUdrQi42dU5tT21jY29YS2cuamlyTUFRc3l5IiwicHJvZmlsZV9pbWFnZSI6IiIsImNvbXBhbnkiOm51bGwsInRpdGxlIjpudWxsLCJpbmR1c3RyeSI6bnVsbCwiY2l0eSI6bnVsbCwic3RhdGUiOm51bGwsImdlbmRlciI6bnVsbCwicmFjZSI6bnVsbCwibGdidHEiOm51bGwsInZldGVyYW4iOm51bGwsImZjbV90b2tlbiI6bnVsbCwiZGV2aWNlX2lkIjoiIiwicHJvdmlkZXJfaWQiOiIiLCJwcm92aWRlcl9uYW1lIjoiRW1haWwiLCJyb2xlIjoidXNlciIsInBvaW50cyI6MCwiZW5jX2tleSI6bnVsbCwiZW5jX2l2IjpudWxsLCJjb3VudHJpZXMiOm51bGwsInByb25vdW5zIjpudWxsLCJkb2IiOm51bGwsImNyZWF0ZWRBdCI6IjIwMjQtMDUtMDZUMTU6NTc6MTYuMDAwWiIsInVwZGF0ZWRBdCI6IjIwMjQtMDUtMDZUMTU6NTc6MTYuMDAwWiJ9LCJpYXQiOjE3MTY5NjQ0MzEsImV4cCI6MTc0ODUwMDQzMX0.tqY_xAviMcS2uTCrYWTfPoueOraL9UPYPDWD_gTMa_E"
+        const response = await fetch(apiUrl, {
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json',
+            // 'Authorization': 'Bearer ' + d.token
+          },
+
+          body: JSON.stringify({ code: code })
+        });
+        if (response.ok) {
+          let Result = await response.json();
+          console.log('Response is', Result)
+          const data = Result;
+          setWebCodeResponse(data);
+          if (data.status === true) {
+            // localStorage.setItem('user',
+            //   JSON.stringify(data.data)
+            // )
+            let plan = data.data.user.plan;
+            if (plan !== null && plan.status === "active") {
+              router.push('/home/cards/Promo/subscriptioncompleted')
+            }
+            else {
+              router.push('/home')
+            }
+
+          } else {
+            setWebCodeError(true)
+            console.log('Web code err')
+          }
+        }
+      } catch (error) {
+        console.log('Error is', error)
+      } finally {
+        setWebCodeLoading(false)
+      }
+    }
+    handleUrlCode();
+  }, [])
 
   //code to call login api
 
@@ -46,13 +100,13 @@ const Page = () => {
             JSON.stringify(data.data)
           )
           let plan = data.data.user.plan;
-          if(plan !== null && plan.status === "active"){
+          if (plan !== null && plan.status === "active") {
             router.push('/home/cards/Promo/subscriptioncompleted')
           }
-          else{
+          else {
             router.push('/home')
           }
-          
+
         } else {
           console.log('Couldnot login bcz', data.message)
           setError(true)
@@ -72,7 +126,7 @@ const Page = () => {
       // setCardLoading(true)
       // const apiUrl = 'https://plurawlapp.com/plurawl/api/users/get_profile';
       const apiUrl = process.env.NEXT_PUBLIC_REACT_APP_ENVIRONMENT === "Development2" ? "https://bf59-119-156-82-235.ngrok-free.app/api/users/get_profile" : "https://plurawlapp.com/plurawl/api/users/get_profile";
-      const data = localStorage.getItem('user')
+      const data = localStorage.getItem('user');
       const d = JSON.parse(data);
       const response = await fetch(apiUrl, {
         method: 'post',
@@ -114,9 +168,16 @@ const Page = () => {
     }
   }
 
+  //code for description screen
+  const handleDescriptionClick = () => {
+    router.push('/AddDescription')
+    console.log('Btn is working')
+  }
+
   //code for getting data from local storage
   const handleClose1 = () => {
-    setError(false)
+    setError(false);
+    setWebCodeError(false);
   }
   useEffect(() => {
 
@@ -137,7 +198,7 @@ const Page = () => {
 
         if (response.ok) {
           const cards = await response.json();
-          console.log('Data of user is', cards)
+          console.log('Data of user is', cards);
           if (cards.status === true) {
             let p = cards.data;
             d.user = p;
@@ -155,7 +216,7 @@ const Page = () => {
               router.replace('/home')
             }
           } else {
-
+            console.log('')
           }
         } else {
           console.log('Error', response)
@@ -187,42 +248,72 @@ const Page = () => {
   return (
     // <Suspense>
     <div className="w-full" style={{ backgroundColor: 'black', height: '100vh', display: 'flex', justifyContent: 'center' }}>
-      <div style={{ width: '350px', gap: 150, display: 'flex', flexDirection: 'column' }}>
-        <div className="text-lg	font-semibold" style={{ color: 'white', marginTop: 20 }}>
-          Enter email and password
-        </div>
-        <Snackbar
-          open={error}
-          autoHideDuration={5000}
-          onClose={handleClose1}
-          anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'right'
-          }}
-          TransitionComponent={Slide}
-          TransitionProps={{
-            direction: 'left'
-          }}
-        >
-          <Alert onClose={handleClose1} severity="success" sx={{ width: '50%' }}>
-            {snackMessage}
-          </Alert>
-        </Snackbar>
-        <div>
-          <form style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-            <input onChange={(e) => setEmail(e.target.value)} className='w-5/6 font-semibold' style={{ outline: 'none', padding: '15px', borderRadius: 10 }} type="email" placeholder="Enter Email" />
-            <input onChange={(e) => setPassword(e.target.value)} className='w-5/6 mt-4 font-semibold' style={{ outline: 'none', padding: '15px', borderRadius: 10 }} type="password" placeholder="Enter Password" />
-            <div>
+      {
+        WebCodeLoading ?
+          <div style={{ color: 'red', display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh', width: '100%' }}>
+            <CircularProgress sx={{ height: '30px' }} />
+          </div> :
+          <div style={{ width: '350px', gap: 150, display: 'flex', flexDirection: 'column' }}>
+            <div className="text-lg	font-semibold" style={{ color: 'white', marginTop: 20 }}>
+              Enter email and password
             </div>
-            <div>
-              <button className='font-semibold' onClick={handleSigninclick} style={{ color: 'white', backgroundColor: 'red', height: '50px', width: '100px', borderRadius: 5, marginTop: 30 }}>
-                {loading ? <div style={{ height: '15px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}><CircularProgress sx={{ height: '30px' }} /></div> : 'Sign in'}
-              </button>
-            </div>
-          </form>
-        </div>
+            <Snackbar
+              open={error}
+              autoHideDuration={5000}
+              onClose={handleClose1}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right'
+              }}
+              TransitionComponent={Slide}
+              TransitionProps={{
+                direction: 'left'
+              }}
+            >
+              <Alert onClose={handleClose1} severity="success" sx={{ width: '50%' }}>
+                {snackMessage}
+              </Alert>
+            </Snackbar>
 
-      </div>
+            <Snackbar
+              open={WebCodeError}
+              autoHideDuration={5000}
+              onClose={handleClose1}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right'
+              }}
+              TransitionComponent={Slide}
+              TransitionProps={{
+                direction: 'left'
+              }}
+            >
+              <Alert onClose={handleClose1} severity="error" sx={{ width: '100%' }}>
+                {WebCodeResponse.message} <p>Login in with Email</p>
+              </Alert>
+            </Snackbar>
+
+            <div>
+              <form style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                <input onChange={(e) => setEmail(e.target.value)} className='w-5/6 font-semibold' style={{ outline: 'none', padding: '15px', borderRadius: 10 }} type="email" placeholder="Enter Email" />
+                <input onChange={(e) => setPassword(e.target.value)} className='w-5/6 mt-4 font-semibold' style={{ outline: 'none', padding: '15px', borderRadius: 10 }} type="password" placeholder="Enter Password" />
+                <div>
+                </div>
+                <div>
+                  <button className='font-semibold' onClick={handleSigninclick} style={{ color: 'white', backgroundColor: 'red', height: '50px', width: '100px', borderRadius: 5, marginTop: 30 }}>
+                    {loading ? <div style={{ height: '15px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}><CircularProgress sx={{ height: '30px' }} /></div> : 'Sign in'}
+                  </button>
+                </div>
+              </form>
+              <div style={{ justifyContent: 'center', marginTop: 40, display: 'flex' }}>
+                <Button variant='contained' onClick={handleDescriptionClick}>
+                  Add Description
+                </Button>
+              </div>
+            </div>
+
+          </div>
+      }
     </div>
     // </Suspense>
   )
